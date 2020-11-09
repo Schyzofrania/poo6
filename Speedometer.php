e0f0da82ead8c6154333d908f221b96f0edc2c7e
Schyzofrania <steve.crepin>
2020-11-09 11:12:07 +0100

    poo6

---
+++ Speedometer.php
@@ -0,0 +1,16 @@
+<?php
+
+class Speedometer
+{
+    const milesToKm = 1.69;
+    const kmToMiles = 0.6215;
+
+    public static function kmToMiles($km)
+    {
+        return $km = self::kmToMiles;
+    }
+    public static function milesToKm($miles)
+    {
+        return $miles = self::milesToKm;
+    }
+}
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+require 'Spedometer.php';
+
+echo Spedometer::kmToMiles(10);
